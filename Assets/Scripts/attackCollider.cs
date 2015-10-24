bbc74c4a5b8bd46006417b015bced92524c421c5
LamentedHarp <longjof@woh.rr.com>
2015-10-24 01:28:02 -0400

    The player got scaled, so the attack collider needed changed.

--- Assets/Scripts/attackCollider.cs
+++ Assets/Scripts/attackCollider.cs
@@ -10,7 +10,7 @@ public class attackCollider : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		startPosition = this.transform.position;
-		attackRange = 1.0f;
+		attackRange = 2.0f;
 	}
 	
 	// Update is called once per frame
