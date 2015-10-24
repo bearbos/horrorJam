0ce72e1a58d2bba9336f79dd18d0e9687a42d0f7
MasaKurt <mr.masakurt@gmail.com>
2015-10-24 17:46:54 -0400

    Projectiles done and revert death

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,13 +4,22 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 
     Rigidbody2D rb;
+    [SerializeField]
+    Vector2 Distance;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Distance);
+        Invoke("ZeroGravity", 1.4f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    void ZeroGravity()
+    {
+        rb.gravityScale = 0.0f;
+        rb.velocity = Vector2.zero;
+    }
 }
