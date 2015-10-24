4e13a3a59ec81a19faa98189048759e81f0eab7f
LamentedHarp <longjof@woh.rr.com>
2015-10-23 23:35:05 -0400

    Player attacks fire colliders that check collision and deal damage to objects hit.

--- Assets/Scripts/playerStats.cs
+++ Assets/Scripts/playerStats.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class playerStats : MonoBehaviour {
 
+	public float baseDamage;
+
 	// Use this for initialization
 	void Start () {
-	
+		baseDamage = 20.0f;
 	}
 	
 	// Update is called once per frame
