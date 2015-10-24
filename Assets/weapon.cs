4aaf4a01298bf33caf03c8e459cfd37a955c77ea
MasaKurt <mr.masakurt@gmail.com>
2015-10-24 04:11:41 -0400

    Had more kids and gave them weapons

---
+++ Assets/weapon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weapon : MonoBehaviour {
+
+    public float damage;
+    public int price;
+    public string description;
+    public int durability;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
