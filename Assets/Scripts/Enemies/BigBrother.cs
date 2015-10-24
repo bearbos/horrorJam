aa8a086a206c3294e1ce9f903bbc800f5c500b75
Christopher Conklin <aa>
2015-10-23 22:43:30 -0400

    awdawdawdwa

--- Assets/Scripts/Enemies/BigBrother.cs
+++ Assets/Scripts/Enemies/BigBrother.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class E_Stat : MonoBehaviour {
+public class BigBrother : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/Enemies/E_Stat.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class E_Stat : MonoBehaviour {
+
+    [SerializeField]
+    float currHealth;
+    [SerializeField]
+    float maxHealth;
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   
+    public void TakeDamage(float _dam)
+    {
+        currHealth -= _dam;
+
+        if (currHealth <= 0)
+            gameObject.SendMessage("Death");
+        
+    }
+}
